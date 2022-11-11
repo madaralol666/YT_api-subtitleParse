cde3c17e4a127c9862eb5a88c2c8c97451d25526
madaralol666 <ds409060@gmail.com>
2022-11-11 16:58:17 +0300

    work but fix line\n

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+import json
 
 #кастом лист с языками
 def custom_list(video_id):
@@ -12,14 +13,32 @@ def custom_list(video_id):
         .strip()
 
 
-video_url = input()
-video_url_id = video_url.split('=')[1]
-
-print(custom_list(video_url_id))
-
+# video_url = input()
+# video_url_id = video_url.split('=')[1]
+#
+# print(custom_list(video_url_id))
+#
+#
 # sub_titles = YouTubeTranscriptApi.get_transcript(video_url_id)
-
-
-# with open('subtitles.json', 'w') as file:
+#
+# #Без форматирования
+# with open('subtitles_not_formatted.txt', 'w') as file_snf:
 #     for item in sub_titles:
-#         file.write(f'{item}\n')
+#         file_snf.writelines(f"{item['text']}\n")
+
+#форматирование строк
+with open('subtitles_not_formatted.txt') as file_snf:
+    lines = file_snf.readlines()
+    for a in range(0, len(lines)):
+        if len(lines[a]) >= 43:
+            for i in range(43, len(lines[a])):
+                if lines[a][i] == ' ':
+                    lines[a] = a[:i] + '\n' + a[i + 1:]
+                    break
+
+    non_empty_lines = (line for line in lines if not line.isspace())
+    with open('subtitles_formatted.txt', 'w') as file_formatted:
+        file_formatted.writelines(non_empty_lines)
+
+
+
