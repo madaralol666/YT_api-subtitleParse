bf5d218e2ac121d0319e2e67b81fcd57bb05ce9b
wolk gost <ds409060@gmail.com>
2023-01-23 21:51:42 +0300

    Fix repost

--- main.py
+++ main.py
@@ -13,13 +13,12 @@ def custom_list(video_id):
 
 
 video_url = input()
-video_url_id = video_url.split('=')[1]
 
-print(custom_list(video_url_id))
+state_id = video_url.split('be')[1][1:] if '.be' in video_url else video_url.split('/')[3][8:]
 
-# sub_titles = YouTubeTranscriptApi.get_transcript(video_url_id)
+sub_titles = YouTubeTranscriptApi.get_transcript(state_id)
 
 
-# with open('subtitles.json', 'w') as file:
-#     for item in sub_titles:
-#         file.write(f'{item}\n')
+with open('subtitles.json', 'w') as file:
+    for item in sub_titles:
+        file.write(f'{item}\n')
