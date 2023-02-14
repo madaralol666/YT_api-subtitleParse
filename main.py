23690102a9ee6de299b3f044cc42c58ebd2c6ff7
Zettaisha <70265306+Zettaisha@users.noreply.github.com>
2023-02-14 21:19:12 +0300

    prepare

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ def custom_list(video_id):
         .strip()
 
 
-video_url = input()
+video_url = "https://www.youtube.com/watch?v=1S0aBV-Waeo&ab_channel=Computerphile"
 
 state_id = video_url.split('be')[1][1:] if '.be' in video_url else video_url.split('/')[3][8:]
 
 sub_titles = YouTubeTranscriptApi.get_transcript(state_id)
 
-
-with open('subtitles.json', 'w') as file:
-    for item in sub_titles:
-        file.write(f'{item}\n')
+with open('subtitles.txt', 'w') as file:
+    for line in sub_titles:
+        file.write(f'{line}\n')
